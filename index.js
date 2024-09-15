6f58f2176f0503545b631ce2a6eb9960967b9341
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-09-15 13:48:57 -0700

    Revert "Proyecto completo"
    This reverts commit 900efa226494c61a38fdad80d1f75ba1e33dd757.

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const taskRoutes = require('./Routes/taskRoutes');
-const app = express();
-const PORT = 3000;
-app.use(bodyParser.json());
-app.use('/tasks', taskRoutes);
-
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
